b3f5df5a3e406ba05dbd8cc34f6f3005ad718509
Sarah Cooke <marshcooke1@gmail.com>
2017-09-04 23:57:13 -0500

    frustrated with jq and not running

--- scripts/script.js
+++ scripts/script.js
@@ -3,7 +3,31 @@ console.log('js');
 // when document is ready, run this function
 function onReady() {
     console.log('jq');
+    $('#submit').on('click', calcInput);
 }
+
+//create array for input data
+var employees = [];
+
+// Takes inputs and creates an object constructor, calculates monthly 
+// salary cost, appends to DOM
+function calcInput() {
+    var newEmployee = new Employee ($('#firstIn').val(), $('lastIn').val(), $('jobTitleIn').val(), $('idNumIn').val(), $('annualSalIn'));
+    employees.push(newEmployee);
+    return $('firstIn') + $('lastIn');
+}
+
+
+
+// Object Constructor
+function Employee(firstIn, lastIn, jobTitleIn, idNumIn, annualSalIn) {
+    this.firstName = firstIn;
+    this.lastName = lastIn;
+    this.jobTitle = jobTitleIn;
+    this.idNum = idNumIn;
+    this.annualSalary = annualSalIn;
+}
+
 
 // calling document ready function
 $(document).ready(onReady);
