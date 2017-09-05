6539c8bedbe49b92a8d4108b768c65b03415c09e
Sarah Cooke <marshcooke1@gmail.com>
2017-09-05 07:31:23 -0500

    getting jq to work

--- scripts/script.js
+++ scripts/script.js
@@ -1,20 +1,36 @@
 console.log('js');
 
 // when document is ready, run this function
-function onReady() {
+function readyNow() {
     console.log('jq');
     $('#submit').on('click', calcInput);
 }
 
-//create array for input data
+// create array for input data
 var employees = [];
+
+// set variables to zero for future calculations
+var annualCost = 0;
+var monthlyCost = 0;
 
 // Takes inputs and creates an object constructor, calculates monthly 
 // salary cost, appends to DOM
 function calcInput() {
-    var newEmployee = new Employee ($('#firstIn').val(), $('lastIn').val(), $('jobTitleIn').val(), $('idNumIn').val(), $('annualSalIn'));
-    employees.push(newEmployee);
-    return $('firstIn') + $('lastIn');
+    // yes this worked: console.log('submitt button working?');
+    var newEmployee = new Employee ($('#firstIn').val(), 
+        $('#lastIn').val(), 
+        $('#jobTitleIn').val(), 
+        $('#idNumIn').val(), 
+        $('#annualSalIn').val());
+
+
+    // clear contents in form fields
+    $('#firstIn').val(''), 
+    $('#lastIn').val(''), 
+    $('#jobTitleIn').val(''), 
+    $('#idNumIn').val(''), 
+    $('#annualSalIn').val('');
+    console.log(employees);
 }
 
 
@@ -26,8 +42,9 @@ function Employee(firstIn, lastIn, jobTitleIn, idNumIn, annualSalIn) {
     this.jobTitle = jobTitleIn;
     this.idNum = idNumIn;
     this.annualSalary = annualSalIn;
+    employees.push(this);
 }
 
 
 // calling document ready function
-$(document).ready(onReady);
+$(document).ready(readyNow);
