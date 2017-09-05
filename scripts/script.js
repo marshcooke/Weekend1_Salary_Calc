0ef21645658c064daedba905f717ea1dac7c8e38
Sarah Cooke <marshcooke1@gmail.com>
2017-09-04 23:30:35 -0500

    html and css nearly ready for js

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,9 @@
-console.log("js");
+console.log('js');
+
+// when document is ready, run this function
+function onReady() {
+    console.log('jq');
+}
+
+// calling document ready function
+$(document).ready(onReady);
